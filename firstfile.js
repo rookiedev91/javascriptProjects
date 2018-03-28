e02f1d508cf31088f953ccc0bb32631b502e267c
rookiedev91 <prashant.ee8@gmail.com>
2018-03-28 12:54:44 +0530

    First commit

---
+++ firstfile.js
@@ -0,0 +1,51 @@
+ var firstName = "Julia. ";
+ var interest = "cats. ";
+ var hobby = "play video games. ";
+
+ var awesomeMessage = "Hi, my name is "+ firstName +"I love " + interest+ "In my spare time, I like to " + hobby;
+ console.log(awesomeMessage);
+ firstName = "James. ";
+ interest = "baseball. ";
+ hobby = "read. ";
+ //awesomeMessage = "Hi, my name is "+ firstName +"I love " + interest+ "In my spare time, I like to " + hobby;
+//eval(awesomeMessage);
+// console.log(new String = eval(awesomeMessage));
+  console.log(false && false);
+  var education = "a high school diploma";
+
+// set the value of this based on a person's education
+var salary = 0;
+
+switch(education){
+    
+    case "a Doctoral degree":
+    salary+=84396-89960
+    
+    case "a Professional degree":
+    salary+=89960-69732
+    
+    case "a Master's degree":
+    salary+=69732-59124
+    
+    case "a Bachelor's degree":
+    salary+=59124-41496
+    
+    case "an Associate's degree":
+    salary+=41496-35256
+    
+    case "a high school diploma":
+    salary+=35256-25636
+    
+    case "no high school diploma":
+    salary+=25636
+}
+
+console.log("In 2015, a person with " +education +" earned an average of $" + salary + "year.");
+var x=10
+console.log(x=+x);
+
+var x = 10;
+while (x <= 25) {
+  console.log('Printing out x = ' + x);
+  x = x + 2;
+}
